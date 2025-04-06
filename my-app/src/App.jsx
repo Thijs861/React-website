de1b9767ca991b540813cae42868cc0e59f97e97
Thijs <thijsvanwaveren@outlook.com>
2025-04-06 19:15:56 +0200

    Installed Vite via NPM

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+// Dit is een React hook 
+import { useState } from 'react' 
+// Importeer logos en CSS.
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) // 'count' actueel nummer of kliks, 'setCount' knop om het nummer te veranderen. 'useState' start bij 0
 
   return (
     <>
@@ -20,6 +22,9 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button>
+        <button style={{marginLeft: '10px' }} onClick={() => setCount(0)}>
+          Reset
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
